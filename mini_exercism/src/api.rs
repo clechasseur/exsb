eaa2218bf3ca411f00963e2a5d5b84d1b61b2022
Charles Lechasseur <shiftingbeard@gmx.com>
2023-05-29 01:45:18 -0400

    Work on api module

---
+++ mini_exercism/src/api.rs
@@ -0,0 +1,29 @@
+use reqwest::{Client, IntoUrl, Method, RequestBuilder};
+use crate::core::Credentials;
+
+pub mod website;
+
+/// Client class used to query the Exercism APIs.
+pub struct ApiClient {
+    client: Client,
+    credentials: Option<Credentials>,
+}
+
+impl ApiClient {
+    /// Creates an Exercism API client from the given reqwest::Client and credentials.
+    /// If credentials are not specified, the Exercism API will be queried publicly.
+    pub fn new(client: Client, credentials: Option<Credentials>) -> Self {
+        Self { client, credentials }
+    }
+
+    /// Creates a RequestBuilder used to send a request to an Exercism API.
+    /// Takes care of setting the authorization headers using the credentials
+    /// provided to the constructor, if any.
+    pub fn request<U: IntoUrl>(&self, method: Method, url: U) -> RequestBuilder {
+        let builder = self.client.request(method, url);
+        match &self.credentials {
+            Some(creds) => builder.bearer_auth(creds.api_token()),
+            None => builder,
+        }
+    }
+}
